dcfafdb20076855097a9603acfd575b59bcc7451
suyash <suyashawari0909@gmail.com>
2025-03-08 17:52:53 +0530

    project initialized

---
+++ src/main/java/com/education/edutasker/EduTaskerApplication.java
@@ -0,0 +1,13 @@
+package com.education.edutasker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduTaskerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduTaskerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/education/edutasker/EduTaskerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.education.edutasker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTaskerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
